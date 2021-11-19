b963cda270cf6fdefcf14dd97c91bdbe58be05a8
KJH <stonehead334@gmail.com>
2021-11-19 15:26:27 +0900

    RightHand Logic

--- Maze/ConsoleHelper.cpp
+++ Maze/ConsoleHelper.cpp
@@ -1,14 +1,11 @@
 #include "pch.h"
 #include "ConsoleHelper.h"
 
-
 void ConsoleHelper::SetCursorPosition(int32 x, int32 y)
 {
 	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD pos = { static_cast<SHORT>(x), static_cast<SHORT>(y) };
 	::SetConsoleCursorPosition(output, pos);
-
-
 }
 
 void ConsoleHelper::SetCursorColor(ConsoleColor color)
