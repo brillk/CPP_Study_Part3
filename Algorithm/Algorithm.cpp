4722506d85b86de70ea19f6c799afe5c6ac22a83
KJH <stonehead334@gmail.com>
2021-11-19 17:27:23 +0900

    List Practice

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,21 +1,213 @@
-﻿// Algorithm.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
+#include <list>
 using namespace std;
+
+template<typename T>
+class Node
+{
+public:
+	Node() : _prev(nullptr), _next(nullptr), _data(T())
+	{
+
+	}
+
+	Node(const T& value) : _prev(nullptr), _next(nullptr), _data(value)
+	{
+
+	}
+
+public:
+	Node* _prev;
+	Node* _next;
+	T		_data;
+};
+
+template<typename T>
+class Iterator
+{
+public:
+	Iterator() : _node(nullptr)
+	{
+
+	}
+
+	Iterator(Node<T>* node) : _node(node)
+	{
+
+	}
+
+	// ++it
+	Iterator& operator++()
+	{
+		_node = _node->_next;
+		return *this;
+	}
+
+	// it++
+	Iterator operator++(int)
+	{
+		Iterator<T> temp = *this;
+		_node = _node->_next;
+		return temp;
+	}
+
+	// --it
+	Iterator& operator--()
+	{
+		_node = _node->_prev;
+		return *this;
+	}
+
+	// it--
+	Iterator operator--(int)
+	{
+		Iterator<T> temp = *this;
+		_node = _node->_prev;
+		return temp;
+	}
+
+	// *it
+	T& operator*()
+	{
+		return _node->_data;
+	}
+
+	bool operator==(const Iterator& other)
+	{
+		return _node == other._node;
+	}
+
+	bool operator!=(const Iterator& other)
+	{
+		return _node != other._node;
+	}
+
+public:
+	Node<T>* _node;
+};
+
+template<typename T>
+class List
+{
+public:
+	List() : _size(0)
+	{
+		// [head] <-> ... <-> [tail]
+		_head = new Node<T>();
+		_tail = new Node<T>();
+		_head->_next = _tail;
+		_tail->_prev = _head;
+	}
+
+	~List()
+	{
+		while (_size > 0)
+			pop_back();
+
+		delete _head;
+		delete _tail;
+	}
+
+	void push_back(const T& value)
+	{
+		AddNode(_tail, value);
+	}
+
+	void pop_back()
+	{
+		RemoveNode(_tail->_prev);
+	}
+
+private:
+
+	// [head] <-> [1] <-> [prevNode] <-> [before] <-> [tail]
+	// [head] <-> [1] <-> [prevNode] <-> [newNode] <-> [before] <-> [tail]
+	Node<T>* AddNode(Node<T>* before, const T& value)
+	{
+		Node<T>* newNode = new Node<T>(value);
+		Node<T>* prevNode = before->_prev;
+
+		prevNode->_next = newNode;
+		newNode->_prev = prevNode;
+
+		newNode->_next = before;
+		before->_prev = newNode;
+
+		_size++;
+
+		return newNode;
+	}
+
+	// [head] <-> [prevNode] <-> [node] <-> [nextNode] <-> [tail]
+	// [head] <-> [prevNode] <-> [nextNode] <-> [tail]
+	Node<T>* RemoveNode(Node<T>* node)
+	{
+		Node<T>* prevNode = node->_prev;
+		Node<T>* nextNode = node->_next;
+
+		prevNode->_next = nextNode;
+		nextNode->_prev = prevNode;
+
+		delete node;
+
+		_size--;
+
+		return nextNode;
+	}
+
+	int size() { return _size; }
+
+public:
+	using iterator = Iterator<T>;
+
+	iterator begin() { return iterator(_head->_next); }
+	iterator end() { return iterator(_tail); }
+
+	// it '앞에' 추가
+	iterator insert(iterator it, const T& value)
+	{
+		Node<T>* node = AddNode(it._node, value);
+		return iterator(node);
+	}
+
+	iterator erase(iterator it)
+	{
+		Node<T>* node = RemoveNode(it._node);
+		return iterator(node);
+	}
+
+private:
+	Node<T>* _head;
+	Node<T>* _tail;
+	int			_size;
+};
 
 int main()
 {
-   cout << "Hello World!\n";
+	List<int> li;
+
+	List<int>::iterator eraseIt;
+	// [ ] <-> [ ] <-> [ ] <-> [ ]  <-> [ ]  <-> [ ]
+	for (int i = 0; i < 10; i++)
+	{
+		if (i == 5)
+		{
+			eraseIt = li.insert(li.end(), i);
+		}
+		else
+		{
+			li.push_back(i);
+		}
+	}
+
+	li.pop_back();
+
+	li.erase(eraseIt);
+
+	for (List<int>::iterator it = li.begin(); it != li.end(); it++)
+	{
+		cout << (*it) << endl;
+	}
+
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
