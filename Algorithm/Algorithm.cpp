4551f1c053e3431cf13d6b067333d9dc634fe2dc
KJH <stonehead334@gmail.com>
2021-11-24 15:13:39 +0900

    Hash Table

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -8,58 +8,141 @@ using namespace std;
 
 // 오늘의 주제 : 정렬
 
-int Partition(vector<int>& v, int left, int right)
+// 오늘의 주제 : 해시 테이블
+
+// Q) map vs hash_map (C++11 표준 unordered_map)
+
+// map : Red-Black Tree
+// - 추가/탐색/삭제 O(logN)
+
+// C# dictionary = C++ map (X)
+// C# dictionary = C++ unordered_map
+
+// hash_map (unordered_map)
+// - 추가/탐색/삭제 O(1)
+
+// 살은 내주고 뼈를 취한다!
+// 메모리를 내주고 속도를 취한다!
+
+// 아파트 우편함
+// [201][202][203][204][205]
+// [101][102][103][104][105]
+
+// 1~999 user(userId=1~999)
+// [1][2][3][][][][][999]
+
+// '해시' '테이블'
+// O(1)
+void TestTable()
 {
-	int pivot = v[left];
-	int low = left + 1;
-	int high = right;
-
-	//0(N)
-	while (low <= high)
+	struct User
 	{
-		while (low <= right && pivot >= v[low])
-			low++;
+		int userId = 0; // 1~999
+		string username;
+	};
 
-		while (high >= left + 1 && pivot <= v[high])
-			high--;
+	vector<User> users;
+	users.resize(1000);
 
-		if (low < high)
-			swap(v[low], v[high]);
+	// 777번 유저 정보 세팅
+	users[777] = User{ 777, "Rookiss" };
+
+	// 777번 유저 이름은?
+	string name = users[777].username;
+	cout << name << endl;
+
+	// 테이블
+	// 키를 알면, 데이터를 단번에 찾을 수 있다!
+
+	// 문제의 상황
+	// int32_max (3억~)
+	// 살을 내주는 것도 정도껏 내줘야지...
+	// 메모리도 무한은 아니다!
+}
+
+// 보안
+// id: rookiss + pw: qwer1234
+// id: rookiss + pw: hash(qwer1234) -> sdaf123!@#asdfasdf1234
+
+// DB [rookiss][sdaf123!@#asdfasdf1234]
+// 비밀번호 찾기 -> 아이디 입력 / 폰 인증 -> 새 비밀번호를 입력하세요
+
+void TestHash()
+{
+	struct User
+	{
+		int userId = 0; // 1~int32_max
+		string username;
+	};
+
+	//       []
+	// [][][][][][][][]
+	vector<User> users;
+	users.resize(1000);
+
+	const int userId = 123456789;
+	int key = (userId % 1000); // hash < 고유번호
+
+	// 123456789번 유저 정보 세팅
+	users[key] = User{ userId, "Rookiss" };
+
+	// 123456789번 유저 이름은?
+	User& user = users[key];
+	if (user.userId == userId)
+	{
+		string name = user.username;
+		cout << name << endl;
 	}
 
-	swap(v[left], v[high]);
-	return high;
+	// 충돌 문제
+	// 충돌이 발생한 자리를 대신해서 다른 빈자리를 찾아나서면 된다
+	// - 선형 조사법 (linear probing)
+	// hash(key)+1 -> hash(key)+2
+	// - 이차 조사법 (quadratic probing)
+	// hash(key)+1^2 -> hash(key)+2^2
+	// - etc
+
+	// 체이닝
+	// 
+
 }
 
-// 최악의 경우 0(N^2)	
-// 평균적 0(N logN)
-void QuickSort(vector<int>& v, int left, int right)
+// O(1)
+void TestHashTableChaining()
 {
-	if (left > right)
-		return;
+	struct User
+	{
+		int userId = 0; // 1~int32_max
+		string username;
+	};
 
-	int pivot = Partition(v, left, right);
-	QuickSort(v, left, pivot - 1);
-	QuickSort(v, pivot + 1, right);
+	// [ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]
+	vector<vector<User>> users;
+	users.resize(1000);
+
+	const int userId = 123456789;
+	int key = (userId % 1000); // hash < 고유번호
+
+	// 123456789번 유저 정보 세팅
+	users[key].push_back(User{ userId, "Rookiss" });
+	users[789].push_back(User{ 789, "FaKer" });
+
+	// 123456789번 유저 이름은?
+	vector<User>& bucket = users[key];
+	for (User& user : bucket)
+	{
+		if (user.userId == userId)
+		{
+			string name = user.username;
+			cout << name << endl;
+		}
+	}
 }
+
+
 
 int main()
 {
-	vector<int> v;
-
-	srand(time(0));
-
-	for (int i = 0; i < 50; i++)
-	{
-		int randValue = rand() % 100;
-		v.push_back(randValue);
-	}
-
-	//BubbleSort(v);
-	//SelectionSort(v);
-	//InsertSort(v);
-	//HeapSort(v);
-	//MergeSort(v, 0, v.size() - 1);
-
-	QuickSort(v, 0, v.size() - 1);
+	//TestTable();
+	TestHash();
 }
