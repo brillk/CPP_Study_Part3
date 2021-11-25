e720d8070bf52d432205ce83bd90457906a2380f
KJH <stonehead334@gmail.com>
2021-11-25 11:23:01 +0900

    DP TRIANGLE_PATH Practice

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -8,62 +8,79 @@
 using namespace std;
 
 //오늘의 주제 : 동적 계획법(DP)
-//LIS Longest Increasing Sequence
 
-//Seq :  1 9 3 5 6
-//부분 수열 : 일부(0개 이상) 숫자를 지우고 남은 수열
-//ex) 1 3 5  -  1 9 5
-//순 증가 부분 수열 - 첫 값부터 오름차순으로 커지는 것
-//ex) 1 3 5  맞음
-//ex) 1 9 5  틀림
+//TRIANGLE_PATH
+// 0,0부터 시작해서 아래 혹은 아래우측으로 이동 가능
+// 만나는 숫자를 모두 더함
+// 더한 숫자가 최대가 되는 경로 
 
-//LIS : 제일 긴 [순 증가 부분 수열] 
-// 1 3 5 6 = 9를 뺸 가장 길이가 긴 부분 수열 4짜리 
+int N;
+vector<vector<int>> board;
+vector<vector<int>> cache;
+vector<vector<int>> nextX;
 
-int cache[100];
-vector<int> seq; 
-
-int LIS(int pos)
+int path(int y, int x)
 {
-	//기저 사항 
-	
+	//기저 사항
+	//if (y == N - 1)
+	//	return board[y][x];
+	if (y == N)
+		return 0;
 
 	//캐시 확인
-	int& ret = cache[pos];
+	int& ret = cache[y][x];
 	if (ret != -1)
 		return ret;
-	//구하기
-	//Seq :  1 9 3 5 6
-	//1 9  
-	//1 3
-	//1 5
-	//1 6 
-	// 
-	// 1이 모든 값보다 작으니까 다음에 올 값은 이렇게 정해져 있는데
-	// 1 9 다음에 숫자가 다 작으니 이것의 크기는 2다
-	// 1 3 5 6 시작하는 인덱스가 점차 커지는 형태면 살을 붙인다 사이즈는 4
-	// 1 5 6 사이즈는 3
-	// 1 6 사이즈는 2 
-	// 시작하는 인덱스부터 시작해 전의 값보다 커야 한다 
-	// 
-	//최소 seq[pos]은 있으니 1부터 시작
 
-	ret = 1;
 
-	//구하기
-	for (int next = pos + 1; next < seq.size(); next++)
-		if (seq[pos] < seq[next]) 
-			ret = max(ret, 1 + LIS(next));
-	return ret;		
+	//적용
+	/*board[y][x] + path(y + 1, x);
+	board[y][x] + path(y + 1, x + 1);	*/
+
+	//경로기록
+	{
+		int nextBottom = path(y + 1, x);
+		int nextBottomRight = path(y + 1, x+1);
+		if (nextBottom > nextBottomRight)
+			nextX[y][x] = x;
+		else
+			nextX[y][x] = x + 1;
+	}
+
+
+	//현재 값에서 다음에 들어가는 최대값을 비교해서 실행
+	return ret = board[y][x] + max(path(y + 1, x), path(y + 1, x + 1));
 }
 
 int main()
 {
-	::memset(cache, -1, sizeof(cache));
-	seq = vector<int>{ 10, 1, 9, 2, 5, 6 };
+	board = vector<vector<int>>
+	{
+		{6},
+		{1, 2},
+		{3, 7, 4},
+		{9, 4, 1, 7},
+		{2, 7, 5, 9, 4}
+	};
 
-	//모든 아이를 한번 씩 순회해야한다
-	int ret = 0;
-	for (int pos = 0; pos < seq.size(); pos++)
-		ret = max(ret, LIS(pos));
+	N = board.size();
+	cache = vector<vector<int>>(N, vector<int>(N, -1));
+	nextX = vector<vector<int>>(N, vector<int>(N));
+
+	int ret = path(0, 0);
+	cout << ret << endl; // 6 2 4 7 9 = 28
+
+
+	//경로 만들기
+	int y = 0;
+	int x = 0;
+
+	while (y < N)
+	{
+		cout << board[y][x] << "->";
+		//6->2->4->7->9
+
+		x = nextX[y][x];
+		y++;
+	}
 }
